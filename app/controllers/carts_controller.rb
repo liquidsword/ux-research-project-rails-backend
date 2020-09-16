1e577b349f3b5b2b1bb50689dcbb39963cb2e336
liquidsword <audenrobertson@gmail.com>
2020-09-15 21:30:52 -0400

    created reviews controller
    added CRUD capabilities to the reviews controller.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,23 +1,27 @@
 class CartsController < ApplicationController
 
-  def index
-
-  end
-
-  def show
-
-  end
-
   def create
-
+    @product = Product.find(params[:id])
+    @cart.add(@product)
+    redirect_to cart_path
   end
 
   def update
+    @product = Product.find(params[:id])
+    @cart.add(@product)
 
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+    end
   end
 
-  def destroy
+  def remove_item
+    @product = Product.find(params[:id])
+    @cart.remove(@product, 1)
 
+    respond_to do |format|
+      format.html { redirect_to cart_path }
+    end
   end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,58 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @reviews = Review.all
   end
 
   def show
+  end
 
+  def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
 
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to @review.product || @review, notice: 'Review successfully created.'}
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-
+    respond_to do |format|
+      if @review.update(review_params)
+        format.html { redirect_to @review, notice: 'Review has been updated!' }
+        format.json { render :show, status: :ok, location: @review }
+      else
+        format.html { render :edit }
+        format.json { render json: @review.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @review.destroy
+    respond_to do |format|
+      format.html { redirect_to reviews_url, notice: 'Review was successfully deleted!' }
+      format.json { head :no_content}
+    end
+  end
 
+private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :product_id)
   end
 end
